3259a01b6a4794261058b9f10d50477ad94a2781
Julian Moreno <julmo611@gmail.com>
2019-06-05 15:31:13 -0700

    Cookies Done

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -8,7 +8,19 @@ import sys
 
 
 def eating_cookies(n, cache=None):
-    pass
+
+    if n <= 1:
+        return 1
+    elif n == 2:
+        return n
+    elif cache and cache[n] > 0:
+        return cache[n]
+    else:
+        if not cache:
+            cache = {i: 0 for i in range(n+1)}
+        cache[n] = eating_cookies(
+            n-1, cache) + eating_cookies(n-2, cache) + eating_cookies(n-3, cache)
+    return cache[n]
 
 
 if __name__ == "__main__":
