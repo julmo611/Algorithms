8675d7ee1e4394a24587ad4d112d735f6f22ec7b
Julian Moreno <julmo611@gmail.com>
2019-06-05 16:09:01 -0700

    Recipe batches Done

--- recipe_batches/recipe_batches.py
+++ recipe_batches/recipe_batches.py
@@ -4,7 +4,14 @@ import math
 
 
 def recipe_batches(recipe, ingredients):
-    pass
+    batches = []
+    if len(recipe) == len(ingredients):
+        for key in recipe:
+            batches.append(ingredients[key]//recipe[key])
+        return min(batches)
+
+    else:
+        return 0
 
 
 if __name__ == '__main__':
